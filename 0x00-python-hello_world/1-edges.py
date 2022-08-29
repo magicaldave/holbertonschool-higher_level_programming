ef44ebce70de3154a85ebc3d04275b5e6c54a397
magicaldave <smokydave@icloud.com>
2022-08-29 13:41:00 -0500

    Edit: PEP8 validation

--- 0x00-python-hello_world/1-edges.py
+++ 0x00-python-hello_world/1-edges.py
@@ -4,5 +4,5 @@ word = "Holberton"
 print("First 3 letters: {}".format(word[0:3]))
 print("Last 2 letters: {}".format(word[-2:]))
 print("Middle word: {}".format(word[1:-1]))
-#Here's
-#Eight lines
+# Here's
+# Eight lines
