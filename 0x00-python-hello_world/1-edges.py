338bdb838adfe9504bde73c2fdb05277dea51b07
magicaldave <smokydave@icloud.com>
2022-08-29 13:40:04 -0500

    Edit: changed string content in 1-edges.py

--- 0x00-python-hello_world/1-edges.py
+++ 0x00-python-hello_world/1-edges.py
@@ -2,7 +2,7 @@
 """1 - edges: Prints edges of words"""
 word = "Holberton"
 print("First 3 letters: {}".format(word[0:3]))
-print("First 3 letters: {}".format(word[-2:]))
-print("First 3 letters: {}".format(word[1:-1]))
+print("Last 2 letters: {}".format(word[-2:]))
+print("Middle word: {}".format(word[1:-1]))
 #Here's
 #Eight lines
