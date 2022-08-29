1a8d6bb80324e11b8fe3da164230c669475d365a
magicaldave <smokydave@icloud.com>
2022-08-29 13:39:09 -0500

    Edit: changed to more portable syntax.

--- 0x00-python-hello_world/1-edges.py
+++ 0x00-python-hello_world/1-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """1 - edges: Prints edges of words"""
 word = "Holberton"
-print(f"First 3 letters: {word[0:3]}")
-print(f"Last 2 letters: {word[-2:]}")
-print(f"Middle word: {word[1:-1]}")
+print("First 3 letters: {}".format(word[0:3]))
+print("First 3 letters: {}".format(word[-2:]))
+print("First 3 letters: {}".format(word[1:-1]))
 #Here's
 #Eight lines
