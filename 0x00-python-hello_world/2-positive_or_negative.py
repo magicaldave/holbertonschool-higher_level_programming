f98ec2c0b0019a309e3625e1f07a84984aaefabc
magicaldave <smokydave@icloud.com>
2022-08-30 16:53:02 -0500

    Add: 2-positive_or_negative.py, which checks the signedness of a number, and changed print_alphabet to use a loop.

---
+++ 0x00-python-hello_world/2-positive_or_negative.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Gets a random integer and determines its signedness"""
+import random
+
+number = random.randint(-10, 10)
+i = 0
+signedness = ["zero", "negative", "positive"]
+if number < 0:
+    i = 1
+elif number > 0:
+    i = 2
+
+print(f'{number} is {signedness[i]}')
--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 """Print alphabet, minus a newline"""
-print("{}".format("abcdefghijklmnopqrstuvwxyz"), end="")
+i = 97
+while i < 123:
+    print("{0:c}".format(i), end="")
+    i += 1
