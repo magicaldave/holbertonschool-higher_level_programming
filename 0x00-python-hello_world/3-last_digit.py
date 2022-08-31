6cb50cc2a1b61b79c3b5976228b720295907327f
Dave Corley <s3kshun-8@pop-os.localdomain>
2022-08-30 23:16:04 -0500

    Add: 3-last_digit.py, which generates a random number and evaluates its last digit.

---
+++ 0x00-python-hello_world/3-last_digit.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Gets random numbers and evaluates the last digit"""
+import random
+number = random.randint(-10000, 10000)
+# YOUR CODE HERE
+i = 0
+lastdig = (abs(number) % 10)  # Capture the last digit
+suff = ["and is 0", "and is greater than 5", "and is less than 6 and not 0"]
+if lastdig != 0:
+    if lastdig > 5:
+        i = 1
+    else:
+        i = 2
+print("Last digit of {} is {} {}".format(number, lastdig, suff[i]))
