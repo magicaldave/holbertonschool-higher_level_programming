698cbc4e0982da7ee66f5df60202fa2a267d1731
Dave Corley <s3kshun-8@pop-os.localdomain>
2022-09-06 18:05:19 -0500

    Edit: changed 1-search_replace.py to one-liner list comprehension

--- 0x03-python-more_data_structures/1-search_replace.py
+++ 0x03-python-more_data_structures/1-search_replace.py
@@ -4,12 +4,4 @@
 
 def search_replace(my_list, search, replace):
     """Find Search in My_List and Overwrite with Replace"""
-    n_list = list.copy(my_list)
-    if n_list.count(search) > 0:
-        for n in n_list:
-            if n == search:
-                i = n_list.index(n)
-                n_list.remove(n)
-                n_list.insert(i, replace)
-
-    return n_list
+    return [n if n != search else replace for n in my_list]
