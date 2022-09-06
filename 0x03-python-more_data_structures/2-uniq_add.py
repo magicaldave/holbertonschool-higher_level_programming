03dba597df2cac74ca449da52a215cdaa8df2c09
Dave Corley <s3kshun-8@pop-os.localdomain>
2022-09-06 18:47:04 -0500

    Add: 2-uniq_add.py, which adds together unique elements of a set, and a gitignore.

---
+++ 0x03-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+"""Unique-Add Module"""
+
+
+def uniq_add(my_list=None):
+    """Adds together only unique elements of a list"""
+    if my_list is None:
+        my_list = []
+    uniq_sum = 0
+    for n in set(my_list):
+        uniq_sum += n
+    return uniq_sum
