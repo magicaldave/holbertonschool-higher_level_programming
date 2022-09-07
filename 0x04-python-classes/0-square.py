dbc4a1ea05cca1526d875d9b8e63c29991521f78
Dave Corley <s3kshun-8@pop-os.localdomain>
2022-09-07 16:20:45 -0500

    Add: project files for 0x04-Python Classes

---
+++ 0x04-python-classes/0-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This is the square class!"""
+
+
+class Square:
+    """This class defines a square"""
+    def __init__(self, size=None):
+        """This initializes a square with a size"""
+        self.__size = size
---
+++ 0x04-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This is the square class!"""
+
+
+class Square:
+    """This class defines a square"""
+    def __init__(self, size=0):
+        """This initializes a square with a size"""
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
