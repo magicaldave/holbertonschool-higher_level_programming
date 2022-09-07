190908872e5789fed6338799c5cb32c8dba586e2
Dave Corley <s3kshun-8@pop-os.localdomain>
2022-09-06 22:30:06 -0500

    Add: 7-update_dictionary which adds or updates an element of a dictionary.

---
+++ 0x03-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Update Dict Module"""
+
+
+def update_dictionary(a_dictionary, key, value):
+    """Updates a dictionary with new values passed in"""
+    a_dictionary[key] = value
+    return a_dictionary
