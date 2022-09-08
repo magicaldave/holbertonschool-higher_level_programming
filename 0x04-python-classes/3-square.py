51fcb2241ebb5d3805733aba765ed9e3cde6079c
magicaldave <smokydave@icloud.com>
2022-09-08 11:30:57 -0500

    Add: 3-square.py, which now has getters and setters for ther ziee

---
+++ 0x04-python-classes/3-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""0-Square Module"""
+
+
+class Square:
+    """Square Class!"""
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Returns area of the square"""
+        return self.__size**2
+
+    @property
+    def size(self):
+        "Getter for private attr 'self'"
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """Updates square size, if the input is valid."""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
