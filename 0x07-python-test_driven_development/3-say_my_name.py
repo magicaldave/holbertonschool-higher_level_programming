024a2551448860105cd3e45022b009b9ebd6d6a4
Dave Corley <s3kshun-8@pop-os.localdomain>
2022-09-14 16:14:08 -0500

    Edit: Re-ordered operations in 3-say_my_name.py

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -4,9 +4,10 @@
 
 def say_my_name(first_name, last_name=""):
     """ Function that prints the input names """
-    if type(first_name) != str:
+    if isinstance(first_name, str):
+        if isinstance(last_name, str):
+            print("My name is", first_name, last_name)
+        else:
+            raise TypeError("last_name must be a string")
+    else:
         raise TypeError("first_name must be a string")
-    if type(last_name) != str:
-        raise TypeError("last_name must be a string")
-
-    print("My name is", first_name, last_name)
