e79608a356f2825c98e4140b4f0f6c67d2aa9474
Dave Corley <s3kshun-8@pop-os.localdomain>
2022-09-14 23:48:57 -0500

    Edit: 10-divisible_by_2.py, changed None handling.

--- 0x02-python-data_structures/10-divisible_by_2.py
+++ 0x02-python-data_structures/10-divisible_by_2.py
@@ -6,7 +6,8 @@ def divisible_by_2(my_list=None):
     """"""
     r_list = []
     if my_list == []:
-        my_list = None
-    for n in my_list:
-        r_list += [not bool(n % 2)]
+        return None
+    if my_list is not None:
+        for n in my_list:
+            r_list += [not bool(n % 2)]
     return r_list
