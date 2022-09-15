b97f813c28a04df98fc8165b0ce1dbb907ca0d6a
Dave Corley <s3kshun-8@pop-os.localdomain>
2022-09-14 19:52:37 -0500

    Edit: properly handle empty matrices in 6-print_matrix_integer.py

--- 0x02-python-data_structures/6-print_matrix_integer.py
+++ 0x02-python-data_structures/6-print_matrix_integer.py
@@ -2,17 +2,25 @@
 """Print Matrix Module"""
 
 
-def print_matrix_integer(biglist=None):
+def print_matrix_integer(biglist=[[]]):
     """Prints all elements of a matrix"""
+    if biglist == [[]]:
+        biglist = None
+
     if biglist is not None:
         printed = 0
+
+        # Break the main list into sub-lists
         for sublist in biglist:
             if printed == len(sublist):
                 print("")
                 printed = 0
+
+            # Act on each element of the sublist
             for number in sublist:
                 print("{:d}".format(number), end="")
                 printed += 1
-                if printed < len(biglist):
+                # Only separate by spaces if not at the last item
+                if printed < len(sublist):
                     print(end=" ")
     print("")
