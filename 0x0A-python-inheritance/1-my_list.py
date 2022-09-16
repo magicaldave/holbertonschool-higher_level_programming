b8fde7e0de590c16b2145a284fc75e66ecab8468
magicaldave <smokydave@icloud.com>
2022-09-15 21:01:39 -0500

    Edit: removed extra comments in 1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -10,6 +10,3 @@ class MyList(list):
     def print_sorted(self):
         """Prints itself, in numerical order"""
         print(sorted(self))
-        #What cases do I want to guard against?
-        # The list could be: None,
-        # have NaN elements
