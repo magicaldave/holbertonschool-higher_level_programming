f07b36bf46925108aea79332de8876dd526e32ff
magicaldave <smokydave@icloud.com>
2022-09-15 21:56:43 -0500

    Add: last four project files

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" 7-base_geometry Module """
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ Square class that extends Rectangle """
+
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """ returns the area of the Square"""
+        return self.__size**2
+
+    def __str__(self):
+        """ prints square dimensions """
+        return ("[Rectangle] {}/{}".format(self.__size, self.__size))
---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" 7-base_geometry Module """
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ Rectangle class that extends BaseGeometry """
+
+    def __init__(self, width, height):
+        """ initializer for Rectangle class """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
