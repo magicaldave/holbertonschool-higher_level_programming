70cee7c08753dabecc37dc3fc50a869cc01954f4
magicaldave <smokydave@icloud.com>
2022-09-15 21:20:05 -0500

    Add: 6-base_geometry.py, which adds a square class with area function.

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+BaseGeometry initial module
+"""
+
+
+class BaseGeometry():
+    """Nobody expects the Spanish Inquisition!"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
