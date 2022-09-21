77aa5703cc8e6f05dc97b77244af808859301bac
magicaldave <smokydave@icloud.com>
2022-09-21 11:54:38 -0500

    Edit: Changed comments and added exception types in 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-3-to_json_string module
+Writes a list in JSON format to a text file
 """
 
 # Standard Modules
@@ -10,10 +10,14 @@ load_json = __import__('6-load_from_json_file').load_from_json_file
 save_json = __import__('5-save_to_json_file').save_to_json_file
 l_file = "add_item.json"
 
+# Load it, or empty list if not
 try:
     ArgStack = load_json(l_file)
-except:
+except (FileNotFoundError, TypeError):
     ArgStack = []
+
+# Append individually to prevent nested lists
 for arg in sys.argv[1:]:
     ArgStack.append(arg)
+
 save_json(ArgStack, l_file)
