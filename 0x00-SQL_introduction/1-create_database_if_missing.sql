2970ba513dfc4e51ff6345921c4ae7c5b2cba0af
Dave Corley <s3kshun-8@pop-os.localdomain>
2022-10-05 23:52:24 -0500

    Add: SQL introduction assignments

---
+++ 0x00-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,3 @@
+-- create database hbtn_0c_0 in MySQL server
+-- creates database even if it already exist
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
---
+++ 0x00-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,3 @@
+-- deletes database hbtn_0c_0 in your MySQL server
+-- if database doesn't exist, your script should not fail
+DROP DATABASE IF EXISTS hbtn_0c_0;
