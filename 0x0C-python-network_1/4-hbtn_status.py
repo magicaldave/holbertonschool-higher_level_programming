1f6903e0eff25ce1b6370d903346a865a447c9db
magicaldave <smokydave@icloud.com>
2022-11-01 16:26:39 -0500

    Edit: 4-hbtn_status.py, removed `with` call

--- 0x0C-python-network_1/4-hbtn_status.py
+++ 0x0C-python-network_1/4-hbtn_status.py
@@ -5,10 +5,9 @@ This module fetches an http site using requests
 from requests import get
 
 if __name__ == '__main__':
-    with get('https://intranet.hbtn.io/status', timeout=10) as response:
-        response = response.text
-        print(
-            'Body response:',
-            '- type: ' + str((type(response))),  # type of object from response
-            '- content: ' + (response),  # raw content
-            sep='\n\t')  # Adds newline and four spaces each time
+    response = get('https://intranet.hbtn.io/status', timeout=10).text
+    print(
+        'Body response:',
+        '- type: ' + str((type(response))),  # type of object from response
+        '- content: ' + (response),  # raw content
+        sep='\n\t')  # Adds newline and four spaces each time
