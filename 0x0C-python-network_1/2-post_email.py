03c3da8bc8cfe8ad76f1f7da3f6d8761e275aab4
magicaldave <smokydave@icloud.com>
2022-11-01 14:15:30 -0500

    Edit: added read() to print statement

--- 0x0C-python-network_1/2-post_email.py
+++ 0x0C-python-network_1/2-post_email.py
@@ -11,4 +11,4 @@ if __name__ == '__main__':
     data = data.encode('ascii')
     req = request.Request(argv[1], data)
     with request.urlopen(req) as response:
-        print(response.decode('utf-8'))
+        print(response.read().decode('utf-8'))
