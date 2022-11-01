78641d8d9be91675b91fac559f5e1b34ea2584aa
magicaldave <smokydave@icloud.com>
2022-11-01 14:33:59 -0500

    Edit: converted to four separate print statements

--- 0x0C-python-network_1/0-hbtn_status.py
+++ 0x0C-python-network_1/0-hbtn_status.py
@@ -4,9 +4,9 @@ from urllib import request
 
 with request.urlopen('https://intranet.hbtn.io/status') as response:
     response = response.read()
-    print(
-        'Body response:',
-        '- type: {}'.format(type(response)),  # type of object from response
-        '- content: {}'.format(response),  # raw content
-        '- utf8 content: {}'.format(response.decode('utf8')),  # utf8 content
-        sep='\n\t')  # Adds newline and four spaces each time
+    print('Body response:')
+    print('\t- type: {}'.format(
+        type(response)))  # type of object from response
+    print('\t- content: {}'.format(response))  # raw content
+    print('\t- utf8 content: {}'.format(
+        response.decode('utf8')))  # Adds newline and four spaces each time
