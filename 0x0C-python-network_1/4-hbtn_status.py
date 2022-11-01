dcae6519dc3df291dc149b8c677b9f68d264860f
magicaldave <smokydave@icloud.com>
2022-11-01 16:14:40 -0500

    Edit: 4-hbtn_status.py, reformatted comments and added if __name__ == '__main__'

--- 0x0C-python-network_1/4-hbtn_status.py
+++ 0x0C-python-network_1/4-hbtn_status.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
-"""This module fetches the status of an http site"""
+"""
+This module fetches an http site using requests
+"""
 import requests
 
-with requests.get('https://intranet.hbtn.io/status', timeout=10) as response:
-    print(
-        'Body response:',
-        '- type: {}'.format(type(
-            response.content.decode('utf-8'))),  # type of object from response
-        '- content: {}'.format(
-            response.content.decode('utf-8')),  # raw content
-        sep='\n\t')  # Adds newline and four spaces each time
+if __name__ == '__main__':
+    with requests.get('https://intranet.hbtn.io/status',
+                      timeout=10) as response:
+        response = response.content.decode('utf-8')
+        print(
+            'Body response:',
+            '- type: ' + str((type(response))),  # type of object from response
+            '- content: ' + (response),  # raw content
+            sep='\n\t')  # Adds newline and four spaces each time
