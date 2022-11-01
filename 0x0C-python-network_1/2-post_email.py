a075e532acd16c0cb788925addd099ab4887914d
magicaldave <smokydave@icloud.com>
2022-11-01 14:14:45 -0500

    Edit: decoded response in 2-post_email.py

--- 0x0C-python-network_1/2-post_email.py
+++ 0x0C-python-network_1/2-post_email.py
@@ -11,4 +11,4 @@ if __name__ == '__main__':
     data = data.encode('ascii')
     req = request.Request(argv[1], data)
     with request.urlopen(req) as response:
-        print(response.read())
+        print(response.decode('utf-8'))
