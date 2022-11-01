d37573fe8a54603c5b4d37200dd2b7dbcd2e9226
magicaldave <smokydave@icloud.com>
2022-11-01 12:52:58 -0500

    Edit: switched from 4 spaces to tab in 0-hbtn_status.py

--- 0x0C-python-network_1/0-hbtn_status.py
+++ 0x0C-python-network_1/0-hbtn_status.py
@@ -9,4 +9,4 @@ with request.urlopen('https://intranet.hbtn.io/status') as response:
         '- type: ' + str(type(response)),  # type of object from response
         '- content: ' + str(response),  # raw content
         '- utf8 content: ' + response.decode('utf-8'),  # utf8 content
-        sep='\n' + ' ' * 4)  # Adds newline and four spaces each time
+        sep='\n\t')  # Adds newline and four spaces each time
