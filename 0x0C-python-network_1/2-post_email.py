5f2a2f55addad13dbe759c6ab260887f4256437f
magicaldave <smokydave@icloud.com>
2022-11-01 14:09:41 -0500

    Add: 2-post_email.py, which gets a specific email from a given domain.

---
+++ 0x0C-python-network_1/2-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This module sends a request to a url and gets a user email
+"""
+from sys import argv
+from urllib import request, parse
+
+if __name__ == '__main__':
+    values = {'email': argv[2]}
+    data = parse.urlencode(values)
+    data = data.encode('ascii')
+    req = request.Request(argv[1], data)
+    with request.urlopen(req) as response:
+        the_page = response.read()
