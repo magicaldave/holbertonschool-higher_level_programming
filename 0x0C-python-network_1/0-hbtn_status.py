244f2d5298ac763729a01feb49cfa811bf59c688
magicaldave <smokydave@icloud.com>
2022-11-01 15:37:07 -0500

    Edit: reverted commits in 0-hbtn_status.py

--- 0x0C-python-network_1/0-hbtn_status.py
+++ 0x0C-python-network_1/0-hbtn_status.py
@@ -2,12 +2,11 @@
 """This module fetches the status of an http site"""
 from urllib import request
 
-if __name__ == '__main__':
-    req = request.Request('https://intranet.hbtn.io/status')
-    with request.urlopen(req) as response:
-        page = response.read()
-        print('Body response:')
-        print('\t- type: {}'.format(type(page)))
-        print('\t- content: {}'.format(page))
-        print('\t- utf8 content: {}'.format(page.decode('utf-8')))
-        # Adds newline and four spaces each time
+with request.urlopen('https://intranet.hbtn.io/status') as response:
+    response = response.read()
+    print(
+        'Body response:',
+        '- type: {}'.format(type(response)),  # type of object from response
+        '- content: {}'.format(response),  # raw content
+        '- utf8 content: {}'.format(response.decode('utf8')),  # utf8 content
+        sep='\n\t')  # Adds newline and four spaces each time
