e5f4ad231c5f83f33c81f26ec441df4c1a3ada6d
magicaldave <smokydave@icloud.com>
2022-11-01 13:06:34 -0500

    Edit: removed if name == main, no diff

--- 0x0C-python-network_1/0-hbtn_status.py
+++ 0x0C-python-network_1/0-hbtn_status.py
@@ -2,12 +2,11 @@
 """This module fetches the status of an http site"""
 from urllib import request
 
-if __name__ == '__main__':
-    with request.urlopen('https://intranet.hbtn.io/status') as response:
-        response = response.read()
-        print(
-            'Body response:',
-            '- type: ' + str(type(response)),  # type of object from response
-            '- content: ' + str(response),  # raw content
-            '- utf8 content: ' + response.decode('utf-8'),  # utf8 content
-            sep='\n\t')  # Adds newline and four spaces each time
+with request.urlopen('https://intranet.hbtn.io/status') as response:
+    response = response.read()
+    print(
+        'Body response:',
+        '- type: ' + str(type(response)),  # type of object from response
+        '- content: ' + str(response),  # raw content
+        '- utf8 content: ' + response.decode('utf-8'),  # utf8 content
+        sep='\n\t')  # Adds newline and four spaces each time
