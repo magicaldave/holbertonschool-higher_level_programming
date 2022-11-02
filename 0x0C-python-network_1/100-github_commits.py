7bbf7ffd87c079f6348ac54742cecdc8185d345b
Dave Corley <s3kshun-8@pop-os.localdomain>
2022-11-01 20:57:55 -0500

    Edit: 100-github_commits.py, removed if __name__ == __main__

--- 0x0C-python-network_1/100-github_commits.py
+++ 0x0C-python-network_1/100-github_commits.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
 """gets the last 10 commits from repo"""
 
-if __name__ == "__main__":
-    from sys import argv
-    import requests
+from sys import argv
+import requests
 
-    commit_list = requests.get(
-        'https://api.github.com/repos/{}/{}/commits'.format(argv[2], argv[1]),
-        params={
-            'per_page': 10,
-            'page': 1
-        },
-        timeout=10).json()
+commit_list = requests.get('https://api.github.com/repos/{}/{}/commits'.format(
+    argv[2], argv[1]),
+                           params={
+                               'per_page': 10,
+                               'page': 1
+                           },
+                           timeout=10).json()
 
-    for commit in commit_list:
-        print(
-            commit.get('sha') + ': ' +
-            commit.get('commit').get('author').get('name'))
+for commit in commit_list:
+    print(
+        commit.get('sha') + ': ' +
+        commit.get('commit').get('author').get('name'))
