a03f32c8ff9b08b53c50018358718104ac9ec8b8
Dave Corley <s3kshun-8@pop-os.localdomain>
2022-12-10 18:54:32 -0600

    Edit: fixed style in 8-script.js

--- 0x03-Javascript-web_jquery/8-script.js
+++ 0x03-Javascript-web_jquery/8-script.js
@@ -1,7 +1,7 @@
 const $ = window.$;
 const swapi = 'https://swapi-api.hbtn.io/api/films/?format=json';
 $.getJSON(swapi, function (data) {
-    $.each(data.results, function(index, film) {
-        $('UL#list_movies').append('<li>' + this.title + '</li>');
-    });
+  $.each(data.results, function (index, film) {
+    $('UL#list_movies').append('<li>' + this.title + '</li>');
+  });
 });
