849bea01375304bfae4a26ad95aa9ca5f4e08f41
Dave Corley <s3kshun-8@pop-os.localdomain>
2022-12-10 19:45:29 -0600

    Edit: switched to getJSON function in 7-script.js to improve readability

--- 0x03-Javascript-web_jquery/7-script.js
+++ 0x03-Javascript-web_jquery/7-script.js
@@ -1,7 +1,7 @@
 const $ = window.$;
 const url = 'https://swapi-api.hbtn.io/api/people/5/?format=json';
 $(document).ready(function () {
-  $.get(url, function (data) {
+  $.getJSON(url, function (data) {
     $('#character').append(data.name);
-  }, 'json');
+  });
 });
